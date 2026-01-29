f0cda841ce5840b9bf320741b942750ceac3981e
jammeh1 <ensajammeh20@gmail.com>
2026-01-28 18:49:57 -0800

    "initial commit"

--- supabase-admin/src/lib/supabase.ts
+++ supabase-admin/src/lib/supabase.ts
@@ -2,8 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 // Initialize database client
 const supabaseUrl = "https://ousrwhwfrwlrnheoavzk.supabase.co";
-const supabaseKey =
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91c3J3aHdmcndscm5oZW9hdnprIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg3Mzk1OTUsImV4cCI6MjA4NDMxNTU5NX0.bsmClz_bzvYTyo0VjnhgOB3Z5qbA0L3WJb8dmBLgLt0";
+const supabaseKey = "sb_publishable_dhSHa8l9iac5n1k9q8zn2w_--Ft9DKh";
 
 // Create client with extended properties for edge function calls
 const supabase = createClient(supabaseUrl, supabaseKey) as ReturnType<
